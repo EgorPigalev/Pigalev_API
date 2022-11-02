9079f67e677c3d80a93b07eed2ab94da1443e1ff
ПигалевЕД <1pigaleve@mail.ru>
2022-11-02 14:33:58 +0300

    Добавлен метод для Сортировки и Поиска

--- Pigalev_API/Controllers/PhonesController.cs
+++ Pigalev_API/Controllers/PhonesController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Pigalev_API.Models;
@@ -36,38 +37,107 @@ namespace Pigalev_API.Controllers
             return Ok(phones);
         }
 
+        // GETSearch: api/Phones
+        [ResponseType(typeof(List<PhoneModel>))]
+        public IHttpActionResult GetPhonesSearch(string fieldSearch, string textSearch, string fieldSort, string valueSort) // Метод для поиска и сортировки
+        {
+            List<Phones> phones = db.Phones.ToList();
+            if (textSearch != null)
+            {
+                if (fieldSort != null)
+                {
+                    phones = Search(phones, fieldSearch, textSearch);
+                    phones = Sorting(phones, fieldSort, valueSort);
+                }
+                else
+                {
+                    phones = Search(phones, fieldSearch, textSearch);
+                }
+            }
+            else
+            {
+                if (fieldSort != null)
+                {
+                    phones = Sorting(phones, fieldSort, valueSort);
+                }
+            }
+            return Ok(phones);
+        }
+
+        public List<Phones> Search(List<Phones> phones, string fieldSearch, string textSearch) // Метод для поиска
+        {
+            Regex regex = new Regex("^(" + textSearch + ")");
+            switch (fieldSearch)
+            {
+                case ("manufacturer"):
+                    phones = phones.Where(x => regex.IsMatch(x.manufacturer)).ToList();
+                    break;
+                case ("model"):
+                    phones = phones.Where(x => regex.IsMatch(x.model)).ToList();
+                    break;
+                case ("colour"):
+                    phones = phones.Where(x => regex.IsMatch(x.colour)).ToList();
+                    break;
+            }
+            return phones;
+        }
+
+        public List<Phones> Sorting(List<Phones> phones, string fieldSort, string valueSort) // Метод для сортировки
+        {
+            switch (fieldSort)
+            {
+                case ("manufacturer"):
+                    if (valueSort == "возрастание")
+                    {
+                        phones = phones.OrderBy(x => x.manufacturer).ToList();
+
+                    }
+                    else
+                    {
+                        phones = phones.OrderByDescending(x => x.manufacturer).ToList();
+                    }
+                    break;
+                case ("model"):
+                    if (valueSort == "возрастание")
+                    {
+                        phones = phones.OrderBy(x => x.model).ToList();
+
+                    }
+                    else
+                    {
+                        phones = phones.OrderByDescending(x => x.model).ToList();
+                    }
+                    break;
+                case ("colour"):
+                    if (valueSort == "возрастание")
+                    {
+                        phones = phones.OrderBy(x => x.colour).ToList();
+
+                    }
+                    else
+                    {
+                        phones = phones.OrderByDescending(x => x.colour).ToList();
+                    }
+                    break;
+                case ("price"):
+                    if (valueSort == "возрастание")
+                    {
+                        phones = phones.OrderBy(x => x.price).ToList();
+
+                    }
+                    else
+                    {
+                        phones = phones.OrderByDescending(x => x.price).ToList();
+                    }
+                    break;
+            }
+            return phones;
+        }
+
         // PUT: api/Phones/5
         [ResponseType(typeof(void))]
         public IHttpActionResult PutPhones(int id, Phones phones)
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return BadRequest(ModelState);
-            //}
-
-            //if (id != phones.id_phone)
-            //{
-            //    return BadRequest();
-            //}
-
-            //db.Entry(phones).State = EntityState.Modified;
-
-            //try
-            //{
-            //    db.SaveChanges();
-            //}
-            //catch (DbUpdateConcurrencyException)
-            //{
-            //    if (!PhonesExists(id))
-            //    {
-            //        return NotFound();
-            //    }
-            //    else
-            //    {
-            //        throw;
-            //    }
-            //}
-
             var dbPhones = db.Phones.FirstOrDefault(x => x.id_phone.Equals(id));
 
             dbPhones.manufacturer = phones.manufacturer;
